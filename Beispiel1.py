054a1e10ccddbc5fcfa66b02fd554f6323d0fa85
SPLLuca <171wita14@o365.htl-leoben.at>
2018-06-04 16:01:26 +0200

    IBAN komplett

--- Beispiel1.py
+++ Beispiel1.py
@@ -1,7 +1,27 @@
 #beispiel1.py
-print ("Hallo Welt")
-print ("Lokale Version")
+land = "AT"
+blz = "38282"
+konto = "3026713"
+check = "96"
+kontoLen = 11
 
-# beispiel1.py
-print ("Hello UNiverse")
-print ("Hallo alle")
+if(len(konto) != kontoLen):
+	difference = kontoLen-len(konto)
+	for i in range(len(konto), kontoLen):
+		konto = "0" + konto
+	
+	
+
+numbers = blz+konto
+counter = 0
+
+print ( land + check, end=" " )
+
+for i in range(0,len(numbers),1):
+	counter+=1
+	print (numbers[i], end="")
+	if(counter == 4):
+		print ( " " , end= "")
+		counter =0
+	
+
