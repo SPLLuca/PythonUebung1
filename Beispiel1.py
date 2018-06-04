dcd56a78f503ef63ba1d52349c6b6d1132e9987a
SPLLuca <171wita14@o365.htl-leoben.at>
2018-06-04 16:27:42 +0200

    check berechnet

--- Beispiel1.py
+++ Beispiel1.py
@@ -1,21 +1,28 @@
 #beispiel1.py
+
+def pruefziffer(code):
+	check = (98-((int(code))%97))
+	return check
+	
 land = "AT"
 blz = "38282"
 konto = "3026713"
-check = "96"
 kontoLen = 11
 
 if(len(konto) != kontoLen):
 	difference = kontoLen-len(konto)
 	for i in range(len(konto), kontoLen):
 		konto = "0" + konto
-	
-	
+
+iban = blz+konto+"1029"+"00"
+check = pruefziffer(iban)
+
+print (check)
 
 numbers = blz+konto
 counter = 0
 
-print ( land + check, end=" " )
+print ( land + str(check), end=" " )
 
 for i in range(0,len(numbers),1):
 	counter+=1
